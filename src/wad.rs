ec315f65fce38144b16b0bbf7583bd77b8295837
Eric <emercier@weborama.com>
2022-10-23 22:13:11 +0200

    Improvements

--- src/wad.rs
+++ src/wad.rs
@@ -1,12 +1,46 @@
+use std::array::TryFromSliceError;
 use std::borrow::Cow;
 use std::{fs, io, str};
 
 const LUMP_HEADER_SIZE: usize = 16;
+
+#[derive(thiserror::Error, Debug)]
+pub enum WadError {
+    #[error("Unreadable file")]
+    UnreadableFile(#[from] io::Error),
+    #[error("Slice with incorrect length {0}")]
+    SliceWithIncorrectLenght(#[from] TryFromSliceError),
+    #[error("Unknown WAD type {0}")]
+    UnkownWadType(String),
+}
+
 #[derive(Debug)]
 pub struct LumpHeader {
     offset: i32,
     size: i32,
     name: String,
+}
+
+impl TryFrom<&[u8; 16]> for LumpHeader {
+    type Error = WadError;
+
+    fn try_from(value: &[u8; 16]) -> Result<Self, Self::Error> {
+        let name = String::from_utf8_lossy(&value[8..16]).into_owned();
+        let data_position = i32::from_le_bytes(value[..4].try_into()?);
+        let size = i32::from_le_bytes(value[4..8].try_into()?);
+
+        let lump_header = LumpHeader {
+            offset: data_position,
+            size,
+            name,
+        };
+        Ok(lump_header)
+    }
+}
+
+#[derive(Debug)]
+pub enum LumpType {
+    THINGS,
 }
 
 #[derive(Debug)]
@@ -17,17 +51,38 @@ pub struct WadHeader {
 }
 
 #[derive(Debug)]
+pub struct Thing {
+    x: i32,
+    y: i32,
+    angle: i32,
+    thing_type: i32,
+    flags: i32,
+}
+
+impl TryFrom<&[u8; 10]> for Thing {
+    type Error = WadError;
+
+    fn try_from(value: &[u8; 10]) -> Result<Self, Self::Error> {
+        let x = i32::from_le_bytes(value[0..2].try_into()?);
+        let y = i32::from_le_bytes(value[2..4].try_into()?);
+        let angle = i32::from_le_bytes(value[4..6].try_into()?);
+        let thing_type = i32::from_le_bytes(value[6..8].try_into()?);
+        let flags = i32::from_le_bytes(value[8..10].try_into()?);
+
+        Ok(Thing {
+            x,
+            y,
+            angle,
+            thing_type,
+            flags,
+        })
+    }
+}
+
+#[derive(Debug)]
 pub enum WadType {
     IWAD,
     PWAD,
-}
-
-#[derive(thiserror::Error, Debug)]
-pub enum WadError {
-    #[error("Unreadable file")]
-    UnreadableFile(#[from] io::Error),
-    #[error("Unknown WAD type {0}")]
-    UnkownWadType(String),
 }
 
 impl TryFrom<Cow<'_, str>> for WadType {
@@ -46,42 +101,22 @@ pub fn parse(wad_path: &str) -> Result<WadHeader, WadError> {
     let source = fs::read(wad_path)?;
     let wad_type = String::from_utf8_lossy(&source[0..4]);
 
-    let lumps_number: i32 = i32::from_le_bytes([source[4], source[5], source[6], source[7]]); // Are we sure?
-    let directory_offset: i32 = i32::from_le_bytes([source[8], source[9], source[10], source[11]]); // Are we sure?
+    let lumps_number: i32 = i32::from_le_bytes(source[4..8].try_into()?);
+    let directory_offset: i32 = i32::from_le_bytes(source[8..12].try_into()?);
     let wad_type = wad_type.try_into()?;
-    
-    
+
     for x in (0..source.len() - directory_offset as usize).step_by(LUMP_HEADER_SIZE) {
         let offset = directory_offset as usize + x;
-
- 
-        let lump_name = String::from_utf8_lossy(&source[offset + 8 .. offset + 16]);
-
-        let sub = [
-            source[offset],
-            source[offset + 1],
-            source[offset + 2],
-            source[offset + 3],
-        ];
-        let data_position = i32::from_le_bytes(sub);
-
-        let sub = [
-            source[offset + 4],
-            source[offset + 5],
-            source[offset + 6],
-            source[offset + 7],
-        ];
-        let lump_size = i32::from_le_bytes(sub);
-
-        let lump_header = LumpHeader{offset: data_position, size: lump_size, name: lump_name.into_owned()};
-        println!("{:?}", lump_header);
+        let slice: &[u8; LUMP_HEADER_SIZE] =
+            &source[offset..offset + LUMP_HEADER_SIZE].try_into()?;
+        let lump: LumpHeader = slice.try_into()?;
+        println!("{:?}", lump);
     }
-    
 
     let wad = WadHeader {
         wad_type,
         lumps_number,
-        directory_offset
+        directory_offset,
     };
     println!("{:?}", wad);
     Ok(wad)
@@ -95,7 +130,7 @@ mod tests {
     #[test]
     pub fn test_read_wad() {
         assert!(matches!(
-            parse("/home/eric/Downloads/DOOM2.WAD"),
+            parse("/home/emercier/Downloads/DOOM2.WAD"),
             Ok(WadHeader {
                 wad_type: WadType::IWAD,
                 lumps_number: 2919,
@@ -107,7 +142,7 @@ mod tests {
     #[test]
     pub fn test_read_invalid_wad() {
         assert!(matches!(
-            parse("/home/eric/Downloads/url.png"),
+            parse("/home/emercier/Downloads/url.png"),
             Err(WadError::UnkownWadType(_))
         ));
     }
